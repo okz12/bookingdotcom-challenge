f2b1dcc072845212b0b5b77cb59808c42ced9b1f
Osman <osmanzubair658@gmail.com>
2020-12-24 01:45:25 +0000

    data loading

---
+++ datautils.py
@@ -0,0 +1,25 @@
+from zipfile import ZipFile
+import pandas as pd
+
+def load():
+    filename = "Data/raw.zip"
+
+    with ZipFile(filename, 'r') as zp:
+        zp.extract('booking_train_set.csv', 'Data/')
+
+    df = pd.read_csv("data/booking_train_set.csv", index_col=0)
+
+    cats = ['user_id',
+            'city_id',
+            'device_class',
+            'affiliate_id',
+            'booker_country',
+            'hotel_country',
+            'utrip_id']
+    dates = ['checkin', 'checkout']
+    for c in cats:
+        df[c] = df[c].astype('category')
+
+    for d in dates:
+        df[d] = pd.to_datetime(df[d])
+    return df
